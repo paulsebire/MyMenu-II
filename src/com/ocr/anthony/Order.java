9ebc7ec2d3c48c5dedd19a8120a46a31c17535b6
paulsebire <psebire37@gmail.com>
2019-01-22 16:21:52 +0100

    added  method runMenu to order

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,5 +1,7 @@
 package com.ocr.anthony;
 
+import java.util.Scanner;
+
 public class Order {
     /**
      * Display all available menus in the restaurant.
@@ -26,5 +28,14 @@ public class Order {
          default: System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
 
      }
+    }
+    /**
+    * run asking process for a menu
+     */
+    public void runMenu(){
+        this.displayAvailableMenu();
+        Scanner sc = new Scanner(System.in);
+        int nb = sc.nextInt();
+        this.displaySelectedMenu(nb);
     }
 }
